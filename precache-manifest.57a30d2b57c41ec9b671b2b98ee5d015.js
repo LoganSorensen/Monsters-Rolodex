5baa981499083793a93d38267074f5a620b02a24
Logan Sorensen <loganmsorensen@gmail.com>
2019-12-23 21:06:29 -0800

    Updates

---
+++ precache-manifest.57a30d2b57c41ec9b671b2b98ee5d015.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34c9b0ec487fd5e69fc3ddefadc38199",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "775427383762b8712255",
+    "url": "/monsters-rolodex/static/css/main.dc8e0d43.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "775427383762b8712255",
+    "url": "/monsters-rolodex/static/js/main.c6010c86.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
